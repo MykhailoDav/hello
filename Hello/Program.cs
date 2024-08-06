0892dd7d034752f03598c0b0b3ee753935eb20d7
MykhailoDav <davudenko.mukhailo@gmail.com>
2024-08-06 15:32:02 +0300

    create console app

---
+++ Hello/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace HW;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i=0; i < 50; i += 2)
+            Console.WriteLine("Hello world!");
+    }
+}
